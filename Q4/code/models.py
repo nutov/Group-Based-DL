2dc3bc6b6165648140fc870dfcf4c6d155291184
Tuvy <tulemb@gmail.com>
2025-07-31 13:48:09 +0300

    wip

--- Q4/code/models.py
+++ Q4/code/models.py
@@ -36,7 +36,6 @@ class BasePointCloudNet(nn.Module):
         return x
 
     def forward(self, x):
-        x = x.to(self.device)
         x = self.process_input(x)
         x = self.flatten(x)
         return self.mlp(x)
@@ -59,7 +58,6 @@ class SymmetrizationNet(BasePointCloudNet):
     """
 
     def forward(self, x):
-        x = x.to(self.device)
         res = torch.zeros((self.n_classes), device=self.device)
         for perm in torch.permutations(torch.arange(self.n)):
             res += self.mlp(self.flatten(x[list(perm), :]))
@@ -69,7 +67,6 @@ class SampledSymmetrizationNet(BasePointCloudNet):
 
     def forward(self, x):
         n_samples = 20
-        x = x.to(self.device)
         res = torch.zeros((self.n_classes), device=self.device)
         for i in range(n_samples):
             perm = torch.randperm(self.n)
@@ -94,132 +91,133 @@ class LinearEquivariantLayer(BasePointCloudNet):
 class LinearEquivariantNet(BasePointCloudNet):
     def __init__(self, n_in=256, d_in=3, d_out=40):
         super().__init__(n_in, d_in, d_out)
-        self.mlp = nn.Sequential(
+        self.equivariant_mpl = nn.Sequential(
             LinearEquivariantLayer(d_in=d_in, d_out=self.D_HIDDEN_1),
             nn.ReLU(),
             LinearEquivariantLayer(d_in=self.D_HIDDEN_1, d_out=self.D_HIDDEN_2),
             nn.ReLU(),
+        )
+        self.invariant_mpl = nn.Sequential(
             nn.Linear(in_features=self.D_HIDDEN_2, out_features=self.n_classes)
         )
 
     def forward(self, x):
-        x = x.to(self.device)
-        x = torch.sum(x, dim=1)
+        res = torch.tensor(self.equivariant_mpl(x))  # res: (n, D_HIDDEN_2)
+        res = res.mean(dim=0)  # Sum over the points  # res: (D_HIDDEN_2,)
+        res = self.invariant_mpl(res)  # res: (n_classes,)
+        return res
 
     def forward(self, x):
-        x = x.to(self.device)
         x = torch.sum(x, dim=1)  # Sum over the points
         return self.rho(x)
 
-
-class Canonization_Net(nn.Module):
-    def __init__(self,d_in = 10):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear = nn.Sequential(
-            nn.Linear(d_in, 32),
-            nn.ReLU(),
-            nn.Linear(32, 4)
-        )
-
-    def forward(self, x: torch.tensor):
-        """
-        X - R^(Nxd) 
-        canonize by sorting w.r.t norms of the elements in the dataset , 
-        this is permutation invariant  
-        """
-        norms = torch.norm(x, dim=0)
-        _, idx = torch.sort(norms, descending=True, stable=True)
-
-        x = x[idx]
-        x = self.flatten(x)
-
-        return self.linear(x)
-
-
-
-class Symmetrization_Net(nn.Module):
-    def __init__(self,d = 10):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear = nn.Sequential(
-            nn.Linear(d, 32),
-            nn.ReLU(),
-            nn.Linear(32, 4)
-        )
-
-    def forward(self, x):
-        N,_ = x.size()
-        x_ = torch.zeros_like(self.linear(x))
-        elemnts = [k for k in range(N)]
-        for perm in permutations(elemnts):
-            x_ += self.linear(x[perm,:])
-        return x_
-        
-            
-
-class Sampled_Symmetrization_Net(nn.Module):
-    def __init__(self,d = 10,num_samples = 20):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear = nn.Sequential(
-            nn.Linear(d, 32),
-            nn.ReLU(),
-            nn.Linear(32, 4)
-        )
-        self.num_samples = num_samples
-
-    def forward(self, x):
-        N,_ = x.size()
-        x_ = torch.zeros_like(self.linear(x))
-        it = create_permutations_sampled(x,self.num_samples)
-        for perm in it:
-            x_ += self.linear(x[perm,:])
-        
-        return x_ / self.num_samples
-
-
-class Linear_eq_layer(nn.Module):
-    def __init__(self, d_in=10, d_hidden=32):
-        super().__init__()
-        self.w1 = nn.Linear(d_in,d_hidden)
-        self.w2 = nn.Linear(d_in,d_hidden)
-        
-        
-    def forward(self, x):  # x is (n, d_in)
-        return self.w1(x) + self.w2(torch.unsqueeze(torch.sum(x,dim=0),dim=0))
-    
-
-class Linear_eq_Net(nn.Module):
-    def __init__(self, d_in=10, d_hidden=32 , d_out = 4):
-        super().__init__()
-        self.equiv = nn.Sequential(Linear_eq_layer(d_in,d_hidden),
-                                   nn.ReLU(),
-                                   Linear_eq_layer(d_hidden,d_hidden)
-        )
-
-        self.post_pool = nn.Sequential(
-            nn.ReLU(),
-            nn.Linear(d_hidden, d_out))
-        
-        
-    def forward(self, x):  # x is (n, d_in)
-        x_eq = self.equiv(x)
-        x_pool = x_eq.sum(dim=0)
-        return self.post_pool(x_pool)
-
-
-class AugmentedInvariantNet(nn.Module):
-    def __init__(self, d=10, d_hidden=32):
-        super().__init__()
-        self.net = nn.Sequential(
-            #nn.Flatten(),  # input shape (n, d) → (n*d,)
-            nn.Linear(d , d_hidden),  # assuming n = 10
-            nn.ReLU(),
-            nn.Linear(d_hidden, 1)
-        )
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    def forward(self, x):
-        #x = x.to(self.device)
-        #self.net.to(self.device)
-        return self.net(x)
+# TODO: make AugmentedInvariantNet
+# class Canonization_Net(nn.Module):
+#     def __init__(self,d_in = 10):
+#         super().__init__()
+#         self.flatten = nn.Flatten()
+#         self.linear = nn.Sequential(
+#             nn.Linear(d_in, 32),
+#             nn.ReLU(),
+#             nn.Linear(32, 4)
+#         )
+#
+#     def forward(self, x: torch.tensor):
+#         """
+#         X - R^(Nxd)
+#         canonize by sorting w.r.t norms of the elements in the dataset ,
+#         this is permutation invariant
+#         """
+#         norms = torch.norm(x, dim=0)
+#         _, idx = torch.sort(norms, descending=True, stable=True)
+#
+#         x = x[idx]
+#         x = self.flatten(x)
+#
+#         return self.linear(x)
+#
+#
+#
+# class Symmetrization_Net(nn.Module):
+#     def __init__(self,d = 10):
+#         super().__init__()
+#         self.flatten = nn.Flatten()
+#         self.linear = nn.Sequential(
+#             nn.Linear(d, 32),
+#             nn.ReLU(),
+#             nn.Linear(32, 4)
+#         )
+#
+#     def forward(self, x):
+#         N,_ = x.size()
+#         x_ = torch.zeros_like(self.linear(x))
+#         elemnts = [k for k in range(N)]
+#         for perm in permutations(elemnts):
+#             x_ += self.linear(x[perm,:])
+#         return x_
+#
+#
+#
+# class Sampled_Symmetrization_Net(nn.Module):
+#     def __init__(self,d = 10,num_samples = 20):
+#         super().__init__()
+#         self.flatten = nn.Flatten()
+#         self.linear = nn.Sequential(
+#             nn.Linear(d, 32),
+#             nn.ReLU(),
+#             nn.Linear(32, 4)
+#         )
+#         self.num_samples = num_samples
+#
+#     def forward(self, x):
+#         N,_ = x.size()
+#         x_ = torch.zeros_like(self.linear(x))
+#         it = create_permutations_sampled(x,self.num_samples)
+#         for perm in it:
+#             x_ += self.linear(x[perm,:])
+#
+#         return x_ / self.num_samples
+#
+#
+# class Linear_eq_layer(nn.Module):
+#     def __init__(self, d_in=10, d_hidden=32):
+#         super().__init__()
+#         self.w1 = nn.Linear(d_in,d_hidden)
+#         self.w2 = nn.Linear(d_in,d_hidden)
+#
+#
+#     def forward(self, x):  # x is (n, d_in)
+#         return self.w1(x) + self.w2(torch.unsqueeze(torch.sum(x,dim=0),dim=0))
+#
+#
+# class Linear_eq_Net(nn.Module):
+#     def __init__(self, d_in=10, d_hidden=32 , d_out = 4):
+#         super().__init__()
+#         self.equiv = nn.Sequential(Linear_eq_layer(d_in,d_hidden),
+#                                    nn.ReLU(),
+#                                    Linear_eq_layer(d_hidden,d_hidden)
+#         )
+#
+#         self.post_pool = nn.Sequential(
+#             nn.ReLU(),
+#             nn.Linear(d_hidden, d_out))
+#
+#
+#     def forward(self, x):  # x is (n, d_in)
+#         x_eq = self.equiv(x)
+#         x_pool = x_eq.sum(dim=0)
+#         return self.post_pool(x_pool)
+#
+#
+# class AugmentedInvariantNet(nn.Module):
+#     def __init__(self, d=10, d_hidden=32):
+#         super().__init__()
+#         self.net = nn.Sequential(
+#             #nn.Flatten(),  # input shape (n, d) → (n*d,)
+#             nn.Linear(d , d_hidden),  # assuming n = 10
+#             nn.ReLU(),
+#             nn.Linear(d_hidden, 1)
+#         )
+#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     def forward(self, x):
+#         return self.net(x)
